c58664b8a73948597fc81f976647fbbc5198ccc6
murillodominguez <murillo.v.carballo@gmail.com>
2025-05-01 22:43:07 -0300

    start screen fix

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ function App() {
   return (
     <div className="App">
       {gameStage === 'start' && <StartScreen />}
-      {gameStage === 'start' && <StartScreen />}
-      {gameStage === 'start' && <StartScreen />}
+      {gameStage === 'game' && <StartScreen />}
+      {gameStage === 'end' && <StartScreen />}
     </div>
   );
 }
